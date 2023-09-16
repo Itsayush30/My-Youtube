fdea55fae7cdcdfba079853f9c14e2464d9f251c
itsayush30 <itsayush30@gmail.com>
2023-09-17 00:38:09 +0530

    Search Suggestion - Debouncing

--- src/componenets/Head.js
+++ src/componenets/Head.js
@@ -1,51 +1,86 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { toggleMenu } from '../Utils/appSlice';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../Utils/appSlice";
+import { useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../Utils/contants";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    //API call
+    const timer = setTimeout(() => getSearchSugestions(), 200);
 
-    const toggleMenuHandler = () => {
-        dispatch(toggleMenu());
-    }; //dispatch an action
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSugestions = async () => {
+    console.log("Api-" + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    console.log(json[1]);
+  };
+
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  }; //dispatch an action
 
   return (
-    <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
-        <div className='flex col-span-1'>
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
+          alt="menu"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+        />
 
-            <img 
-            onClick={() => toggleMenuHandler()}
-            className='h-8 cursor-pointer'
-            alt='menu'
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
-            />
-
-            <a href='/'>
-            <img
-            className='h-8 mx-2'
-            alt='youtube-logo'
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            alt="youtube-logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
-            />
-            </a>
-            
-        </div>
-        <div className='col-span-10'>
-            <input className='w-1/2 border border-gray-400 p-2 rounded-l-full'
-            type="text"t/>
-            <button className='py-2 px-5 rounded-r-full border border-gray-400 bg-gray-100' >
-        🔍
+          />
+        </a>
+      </div>
+      <div className="col-span-10">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
+        />
+        <button className="py-2 px-5 rounded-r-full border border-gray-400 bg-gray-100">
+          🔍
         </button>
-        </div>
-        <div>
-            <img
-            className='h-8 col-span-1'
-            alt='user'
-            src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-            />
-        </div>
+        {showSuggestions && <div className="absolute bg-white py-2 px-2 w-[705.69px] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestions.map((s) => (
+              <li key={s} className="py-2 px-2 shadow-sm hover:bg-gray-100">
+                🔍 {s}
+              </li>
+            ))}
+          </ul>
+        </div>}
+      </div>
+      <div>
+        <img
+          className="h-8 col-span-1"
+          alt="user"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Head;
